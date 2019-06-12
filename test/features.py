50feb90985a08643fa8eb10a570f545d59576c3a
Harrison Wilde <35736580+HarrisonWilde@users.noreply.github.com>
2019-06-12 17:06:52 +0100

    Removed test directory

--- test/features.py
+++
@@ -1,8 +0,0 @@
-do_step1():
-	print("Do step 1.")
-
-do_step2():
-	print("Do step 2.")
-
-do_step3():
-	print("Do step 3.")
--- test/master.py
+++
@@ -1,9 +0,0 @@
-import features
-
-features.do_step1()
-
-features.do_step2()
-
-features.do_step3()
-
-
